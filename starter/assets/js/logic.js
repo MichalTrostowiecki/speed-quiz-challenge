d03683b87aa79d67336e204afbaf102c0f6f16db
Michal_T <michal.trostowiecki@gmail.com>
2023-12-23 18:12:38 +0000

    Add feedback function, add checkAnswer function to validate correct/wrong answer

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,7 +1,13 @@
 let question = document.getElementById("question-title");
+let questionsDiv = document.getElementById("questions");
 let answerChoices = document.getElementById("choices");
+let startBtn = document.getElementById("start");
+let startScreen = document.getElementById("start-screen");
+let feedback = document.getElementById("feedback");
+
 
 let correctAnswers = 0;
+let questionIndex = 0;
 
 let allQuestions = {
     question1: {
@@ -20,32 +26,76 @@ let allQuestions = {
     }
 }
 
+startBtn.addEventListener("click", function() {
+    startScreen.classList.add("hide");
+    questionsDiv.classList.remove("hide");
+    displayQuestion();
+})
+
+
+
+
 function displayQuestion() {
 
-    
-    let questionIndex = 0;
+    answerChoices.textContent = "";
     let questionKey = Object.keys(allQuestions)[questionIndex];
     let questionData = allQuestions[questionKey];
+    
+    if (!questionData) {
+        alert("There is no more questions");
+        return;
+    }
+
     question.textContent = questionData.question;
-    console.log(questionData);
 
-        
-        for (answerKey in questionData) {
-
-            if (answerKey !== "question") {
-                answerBtn = document.createElement("button");
-                answerBtn.textContent = questionData[answerKey][1]
-                answerChoices.appendChild(answerBtn);
-
-                answerBtn.addEventListener("click", function(event) {
-                    checkAnswer(event);
-                })
-            }
+    for (let answerKey in questionData) {
+        if (answerKey !== "question") {
+            answerBtn = document.createElement("button");
+            answerBtn.textContent = questionData[answerKey][1]
+            answerChoices.appendChild(answerBtn);
+            answerBtn.addEventListener("click", function() {
+                
+                checkAnswer(questionData[answerKey][0]);
+            })
         }
+    }
     
 }
 
-function checkAnswer() {
+function checkAnswer(isCorrect) {
+
+   
+
+    if (isCorrect) {
+        questionIndex++;
+        correctAnswers++;
+        displayFeedback(true);
+    } else {
+        displayFeedback(false);
+    }
+
+    question.textContent = "";
+    answerChoices.textContent = "";
+    
+    displayQuestion();
 
 }
 
+
+function displayFeedback(isCorrect) {
+
+    feedback.classList.remove("hide");
+    if (isCorrect) {
+        feedback.textContent = "This is right answer";
+    } else {
+        feedback.textContent = "This is wrong answer"
+    }
+    
+
+    setTimeout(function () {
+        feedback.classList.add("hide");
+        feedback.textContent = "";
+    },2000);
+
+    
+}
