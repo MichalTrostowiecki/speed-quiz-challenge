cf2b50a96c9d2a8a6d63c64dbc616e2e1dee8d29
Michal_T <michal.trostowiecki@gmail.com>
2023-12-30 19:29:32 +0000

    Import questions into logic.js

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,3 +1,6 @@
+let allQuestions = window.allQuestions;
+
+
 let question = document.getElementById("question-title");
 let questionsDiv = document.getElementById("questions");
 let answerChoices = document.getElementById("choices");
@@ -21,72 +24,7 @@ if (localStorage.getItem("players") === null ) {
     localStorage.setItem("players", JSON.stringify([]));
 }
 
-let allQuestions = {
-    question1: {
-        question: "What is the html language",
-        answer1: [true, "It's markup language"],
-        answer2: [false, "It's dynamic language"],
-        answer3: [false, "It's not a language"],
-        answer4: [false, "It does not exist"],
-    },
 
-    question2: {
-        question: "What is Javascript",
-        answer2: [false, "It's dynamic language"],
-        answer1: [true, "It's markup language"],
-        answer3: [false, "It's not a language"],
-        answer4: [false, "It does not exist"],
-    },
-
-    question3: {
-        question: "Which language is used for creating the structure of web pages?",
-        answer1: [true, "HTML"],
-        answer2: [false, "JavaScript"],
-        answer3: [false, "Python"],
-        answer4: [false, "SQL"],
-    },
-    
-    question4: {
-        question: "What does CSS stand for?",
-        answer1: [true, "Cascading Style Sheets"],
-        answer2: [false, "Computer Style Sheets"],
-        answer3: [false, "Colorful Style Sheets"],
-        answer4: [false, "Creative Style Sheets"],
-    },
-    
-    question5: {
-        question: "Which tag is used to write JavaScript code inside an HTML document?",
-        answer1: [true, "<script>"],
-        answer2: [false, "<javascript>"],
-        answer3: [false, "<js>"],
-        answer4: [false, "<scripting>"],
-    },
-    
-    question6: {
-        question: "What is a variable in programming?",
-        answer1: [true, "A storage location paired with a name"],
-        answer2: [false, "A type of programming language"],
-        answer3: [false, "An error in the code"],
-        answer4: [false, "A tool to debug programs"],
-    },
-    
-    question7: {
-        question: "Which of these is a JavaScript framework?",
-        answer1: [true, "React"],
-        answer2: [false, "Laravel"],
-        answer3: [false, "Django"],
-        answer4: [false, "Flask"],
-    },
-    
-    question8: {
-        question: "What does 'SQL' stand for?",
-        answer1: [true, "Structured Query Language"],
-        answer2: [false, "Sequential Query Language"],
-        answer3: [false, "Simple Query Language"],
-        answer4: [false, "Standard Query Language"],
-    }
-    
-}
 
 startBtn.addEventListener("click", function() {
     startScreen.classList.add("hide");
@@ -135,6 +73,7 @@ function checkAnswer(isCorrect) {
         correctAnswers++;
         displayFeedback(true);
     } else {
+        questionIndex++
         wrongAsnwers++;
         displayFeedback(false);
         timeCount -= 5
@@ -145,7 +84,7 @@ function checkAnswer(isCorrect) {
     answerChoices.textContent = "";
     displayQuestion();
 
-   },100)
+   },200)
 
    
 
@@ -156,9 +95,9 @@ function displayFeedback(isCorrect) {
 
     feedback.classList.remove("hide");
     if (isCorrect) {
-        feedback.textContent = "This is right answer";
+        feedback.textContent = "Correct!";
     } else {
-        feedback.textContent = "This is wrong answer."
+        feedback.textContent = "Wrong!"
     }
     
     setTimeout(function () {
@@ -226,6 +165,7 @@ submitBtn.addEventListener("click", function(event) {
     localStorage.setItem("players", JSON.stringify(playersData));
     document.getElementById("initials").value = "";
 
+    window.open('highscores.html', '_self');
     
 }
 )
